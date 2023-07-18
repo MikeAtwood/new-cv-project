ca4b4532b9e483e56571a4660f8ff18feba0f3ff
Deadpool1 <atwood169@yahoo.com>
2023-07-18 16:02:03 -0500

    Form and General setup

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+import General from "./General";
+
+const Form = () => {
+    return (
+        <div>
+            <div className="input-field">
+                <General />
+            </div>
+        </div>
+    )
+}
+
+export default Form
---
+++ src/components/General.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const General = () => {
+    return (
+        <div>
+            <input
+                placeholder="First Name"
+                style={{ border: 'none' }}
+                type="text"
+                name="firstName"
+            />
+        </div>
+    )
+}
+
+export default General
