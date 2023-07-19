6346e446d198c81486bfdcad06e8a615197f2702
Deadpool1 <atwood169@yahoo.com>
2023-07-19 14:57:27 -0500

    added state in App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+  const [newFormData, setNewFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    schoolName: '',
+    titleOfStudy: '',
+    studyStartDate: '',
+    studyEndDate: '',
+    numExperiences: 0,
+    maxExperiences: 5,
+    experiences: [
+      {
+        id: uuidv4(),
+        companyName: '',
+        position: '',
+        mainTasks: '',
+        jobStartDate: '',
+        jobEndDate: '',
+      }
+    ]
+  })
+
+  
+
   return (
     <div className="App">
       
