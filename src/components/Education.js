e68531d9ab112d60458915b220409132a26350bc
Deadpool1 <atwood169@yahoo.com>
2023-07-20 14:22:02 -0500

    updated Education

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Education = ({ handleInputChange, schoolName }) => {
+const Education = ({ handleInputChange, schoolName, titleOfStudy, studyStartDate, studyEndDate }) => {
     return (
         <div className="input-info">
             <input
@@ -9,6 +9,30 @@ const Education = ({ handleInputChange, schoolName }) => {
                 type="text"
                 name="schoolName"
                 value={schoolName}
+                onChange={handleInputChange}
+            />
+            <input
+                placeholder="Subject"
+                style={{ border: 'none' }}
+                type="text"
+                name="titleOfStudy"
+                value={titleOfStudy}
+                onChange={handleInputChange}
+            />
+            <input
+                placeholder="Start Date"
+                style={{ border: 'none' }}
+                type="date"
+                name="studyStartDate"
+                value={studyStartDate}
+                onChange={handleInputChange}
+            />
+            <input
+                placeholder="End Date"
+                style={{ border: 'none' }}
+                type="date"
+                name="studyEndDate"
+                value={studyEndDate}
                 onChange={handleInputChange}
             />
         </div>
