582add57992706e285f5417aa1d684aa187ffda3
Deadpool1 <atwood169@yahoo.com>
2023-07-20 14:34:14 -0500

    added Exp to Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React from "react";
 import General from "./General";
 import Education from "./Education";
+import Experience from "./Experience";
 
 const Form = ({ handleInputChange }) => {
     return (
@@ -12,6 +13,7 @@ const Form = ({ handleInputChange }) => {
                 <Education 
                     handleInputChange={handleInputChange}
                 />
+                <Experience />
             </div>
         </div>
     )
